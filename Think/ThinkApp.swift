797ce96dda28aab54df31301845bc2a046cedea5
tony-go <gorez.tony@gmail.com>
2021-07-09 19:34:55 +0200

    Initial Commit

---
+++ Think/ThinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkApp.swift
+//  Think
+//
+//  Created by Tony Gorez on 09/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ThinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
