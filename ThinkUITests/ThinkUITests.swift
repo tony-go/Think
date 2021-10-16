f29c4a9ec3292fc84dab27e423bc774cb88a7167
tony-go <gorez.tony@gmail.com>
2021-10-16 19:25:47 +0200

    chore: add new tests

--- ThinkUITests/ThinkUITests.swift
+++ ThinkUITests/ThinkUITests.swift
@@ -9,10 +9,11 @@ import XCTest
 @testable import Think
 
 class ThinkUITests: XCTestCase {
+    var app: XCUIApplication!
     
     func test_GivenApp_WhenLaunched_ThenListInfoShouldAppear() throws {
         // When, Given
-        let app = self.launchApp()
+        let app = self.app!
         
         // Then
         let title = app.staticTexts["Your sounds"]
@@ -24,19 +25,62 @@ class ThinkUITests: XCTestCase {
     
     func test_GivenApp_WhenLaunched_ThenListShouldBeEmpty() throws {
         // When, Given
-        let app = self.launchApp()
+        let app = self.app!
         
         // Then
         let emptyLabel = app.staticTexts["No sound available"]
         
         XCTAssert(emptyLabel.exists)
     }
+    
+    func test_GivenLaunchedApp_WhenTapOnPlusIcon_ThenCreationModalIsShown() throws {
+        // When
+        let app = self.app!
+        
+        // Then
+        let addButton = app.buttons["Add"]
+        addButton.tap();
+        
+        // When
+        let modalTitle = app.staticTexts["Create a new sound"]
+        XCTAssert(modalTitle.exists)
+    }
+    
+    func test_GivenLaunchedApp_WhenCreateNewSound_ThenItAppearsOnList() throws {
+        // When
+        let app = self.app!
+        
+        // When
+        let addButton = app.buttons["Add"]
+        addButton.tap();
+        
+        let titleField = app.textFields["Title"]
+        titleField.tap()
+        titleField.typeText("New sound")
+        
+        let descriptionField = app.textFields["Description"]
+        descriptionField.tap()
+        descriptionField.typeText("This is a new sound")
+        
+        let saveButton = app.buttons["Create"]
+        saveButton.tap()
+        
+        // Then
+        let listTitle = app.staticTexts["Your sounds"]
+        XCTAssert(listTitle.exists)
+        let textLabel = app.staticTexts["New sound"]
+        XCTAssert(textLabel.waitForExistence(timeout: 3))
+    }
 }
 
 extension ThinkUITests {
+    override func setUp() {
+        self.app = self.launchApp()
+    }
+    
     private func launchApp() -> XCUIApplication {
         let app = XCUIApplication()
-        app.launchArguments = ["ui-testing"]
+        app.launchArguments.append("testing")
 
         // When
         app.launch()
