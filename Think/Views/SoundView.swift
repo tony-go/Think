f0193667e2364bbf2fdac311f4882d7d691f0d51
tony-go <gorez.tony@gmail.com>
2021-10-30 20:28:05 +0200

    feat: add base record item

--- Think/Views/SoundView.swift
+++ Think/Views/SoundView.swift
@@ -32,10 +32,47 @@ struct SoundView: View {
             }.padding()
             Spacer()
             ScrollView(.vertical) {
-                ForEach((1...45).reversed(), id: \.self) {
-                        Text("Item \($0)")
-                    }
-            }
+                ForEach((1...45).reversed(), id: \.self) {num in
+                    // FIXME: add lazy stack
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                                .fill(Color("ItemBackground"))
+                         
+                            HStack {
+                                Circle()
+                                    .fill(Color.blue)
+                                    .frame(width: 20, height: 20)
+                                Text("Item \(num)")
+                                Spacer()
+                            }.padding(.horizontal)
+                        }
+                        Spacer().frame(width: 50)
+                        HStack {
+                            Button(action: {}) {
+                                ZStack {
+                                    Circle()
+                                        .fill(Color("ItemBackground"))
+                                        .frame(width: 42, height: 42)
+                                    Image(systemName: "play")
+                                        .foregroundColor(Color("NavigationBarColor"))
+                                        .font(.title2)
+                                }
+                            }
+                            Button(action: {}) {
+                                ZStack {
+                                    Circle()
+                                        .fill(Color("ItemBackground"))
+                                        .frame(width: 42, height: 42)
+                                    Image(systemName: "record.circle")
+                                        .foregroundColor(.red)
+                                        .font(.title2)
+                                }
+                            }
+                        }
+                    }.padding(.horizontal)
+                }
+            }.padding(.horizontal)
             Spacer()
             ActionBar(editAction: self.openEditionModal).frame(alignment: .bottom)
         }
