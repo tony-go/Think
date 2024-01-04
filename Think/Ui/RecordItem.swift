f9fa8a28c1569cd139b38aac1a6bc7448f01cedb
Tony Gorez <gorez.tony@gmail.com>
2024-01-04 18:11:25 +0100

    chore: format RecordItem

--- Think/Ui/RecordItem.swift
+++ Think/Ui/RecordItem.swift
@@ -12,44 +12,44 @@ struct RecordItem: View {
     var position: Int32
 
     var body: some View {
-                                    HStack {
-                                        ZStack {
-                                            RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                                .fill(Color("ButtonBackground"))
-                                            HStack {
-                                                Circle()
-                                                    .fill(Color.blue)
-                                                    .frame(width: 20, height: 20)
-                                                Text("\(label) \(position)")
-                                                Spacer()
-                                            }.padding(.horizontal)
-                                        }
-                                        Spacer().frame(width: 50)
-                                        HStack {
-                                            Button(action: {}) {
-                                                ZStack {
-                                                    Circle()
-                                                        .fill(Color("ButtonBackground"))
-                                                        .frame(width: 42, height: 42)
-                                                    Image(systemName: "play")
-                                                        .foregroundColor(Color("NavigationBarColor"))
-                                                        .font(.title2)
-                                                }
-                                            }
-                                            Button(action: {}) {
-                                                ZStack {
-                                                    Circle()
-                                                        .fill(Color("ButtonBackground"))
-                                                        .frame(width: 42, height: 42)
-                                                    Image(systemName: "record.circle")
-                                                        .foregroundColor(.red)
-                                                        .font(.title2)
-                                                }
-                                           }
-                                        }
-                                    }
-                                    .padding(.horizontal)
-                                    .frame(height: 42)
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12, style: .continuous)
+                    .fill(Color("ButtonBackground"))
+                HStack {
+                    Circle()
+                        .fill(Color.blue)
+                        .frame(width: 20, height: 20)
+                    Text("\(label) \(position)")
+                    Spacer()
+                }.padding(.horizontal)
+            }
+            Spacer().frame(width: 50)
+            HStack {
+                Button(action: {}) {
+                    ZStack {
+                        Circle()
+                            .fill(Color("ButtonBackground"))
+                            .frame(width: 42, height: 42)
+                        Image(systemName: "play")
+                            .foregroundColor(Color("NavigationBarColor"))
+                            .font(.title2)
+                    }
+                }
+                Button(action: {}) {
+                    ZStack {
+                        Circle()
+                            .fill(Color("ButtonBackground"))
+                            .frame(width: 42, height: 42)
+                        Image(systemName: "record.circle")
+                            .foregroundColor(.red)
+                            .font(.title2)
+                    }
+               }
+            }
+        }
+        .padding(.horizontal)
+        .frame(height: 42)
     }
 }
 
